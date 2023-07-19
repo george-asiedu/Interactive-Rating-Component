0e0ed16b1d4f7aa084a795db72d95e177db6c272
George Kwabena Asiedu <asiedug41@gmail.com>
2023-07-19 22:29:18 +0000

    update

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const thankYou = document.querySelector('.thank-you')
-const buttons = document.querySelector('.btn')
+const button = document.querySelectorAll('.btn')
 const submitButton = document.querySelector('.submit-btn')
+const component = document.querySelector('.component')
+
+
+button[0].addEventListener('click', function() {
+    button[0].classList.add('active')
+})
+
+for (const btn of button) {
+    btn.addEventListener('click', function() {
+        btn.classList.add('active')
+    })
+}
+
+submitButton.addEventListener('click', function() {
+    component.style.display = 'none'
+    thankYou.style.display = 'block'
+})
